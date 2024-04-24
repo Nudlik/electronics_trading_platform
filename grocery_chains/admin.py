c03092c592841f939a4201b1cc854022525ef377
Nudlik <gegorp@mail.ru>
2024-04-24 21:38:13 +0700

    добавил акшн на обнуление долгов

--- grocery_chains/admin.py
+++ grocery_chains/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
@@ -9,6 +9,7 @@ from grocery_chains.models import GroceryChain
 class GroceryChainAdmin(admin.ModelAdmin):
     list_display = ['name', 'debt', 'provider_link', 'country', 'city']
     list_filter = ['city']
+    actions = ['clear_debts']
 
     @admin.display(description='поставщик')
     def provider_link(self, obj):
@@ -16,3 +17,8 @@ class GroceryChainAdmin(admin.ModelAdmin):
             return 'Поставщик отсутствует'
         url = reverse('admin:grocery_chains_grocerychain_change', args=[obj.provider.pk])
         return mark_safe(f'<a href="{url}">{obj.provider}</a>')
+
+    @admin.action(description='Обнулить долги поставщиков')
+    def clear_debts(self, request, queryset):
+        queryset.update(debt=0)
+        self.message_user(request, 'Долги обновлены', messages.SUCCESS)
