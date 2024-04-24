7ff95498956f1bb8bb1454299c8bc48a093e207b
Nudlik <gegorp@mail.ru>
2024-04-24 19:06:03 +0700

    добавил модель продуктов

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name='название')
+    model = models.CharField(max_length=255, verbose_name='модель')
+    launch_date = models.DateField(verbose_name='дата выхода')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name} ({self.model})'
