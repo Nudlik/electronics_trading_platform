152a3877ecefa39974947e6ac9a1bbe5f048b956
Nudlik <gegorp@mail.ru>
2024-04-24 22:38:24 +0700

    добавил круд на продукты

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from products import apps
+from products.views import ProductViewSet
 
 app_name = apps.ProductsConfig.name
 
-urlpatterns = []
+router = DefaultRouter()
+router.register('products', ProductViewSet, basename='products')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
